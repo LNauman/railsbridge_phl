59da5ead49ebabc93c63ee44d4efe19ff012781e
Lindsay Nauman <lindsay.nauman@revzilla.com>
2016-08-22 22:48:47 -0400

    more styling

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation();
+
+function toggle_visibility(id) {
+  var e = document.getElementById(id);
+  if(e.style.display == 'block')
+    e.style.display = 'none';
+  else
+    e.style.display = 'block';
+}
